392aa7907471f78c37f156701119c6e3ee1f5cc1
Diogo Andrade <diogo.g.andrade@gmail.com>
2019-11-30 16:28:28 +0000

    First push

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOver : MonoBehaviour
+{
+    public TextMeshProUGUI textGameOver;
+    public TextMeshProUGUI textCompleted;
+
+    void Start()
+    {
+        textGameOver.enabled = false;
+    }
+
+    private void Update()
+    {
+        if ((textGameOver.enabled) || (textCompleted.enabled))
+        {
+            if (Input.anyKeyDown)
+            {
+                SceneManager.LoadScene("Title");
+            }
+        }
+    }
+
+    public void ActivateGameOver()
+    {
+        textGameOver.enabled = true;
+    }
+
+    public void ActivateCompleted()
+    {
+        textCompleted.enabled = true;
+    }
+}
